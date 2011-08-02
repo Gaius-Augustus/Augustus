1c22755b78598eb69214415a762e362acd0d30d9
Lizzy Gerischer <lizzy.gerischer@gmail.com>
2011-08-02 15:08:56 +0000

    completed changes
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@48 946bced8-b911-4d30-a262-e0a769a8c5ad

--- include/mea.hh
+++ include/mea.hh
@@ -2,6 +2,7 @@
 #define _MEA_HH
 
 #include "gene.hh"
+//#include "graph.hh"
 
 list<Gene>* getMEAtranscripts(Gene **sampledGeneStructures, int n, int strlength);
 list<Gene>* getMEAtranscripts(list<Gene> *alltranscripts, int strlength);
@@ -9,4 +10,5 @@ list<Gene>* getMEAtranscripts(list<Gene> *alltranscripts, int strlength);
 Gene* listToGene(list<Gene> *genelist);
 list<Gene>* geneToList(Gene *genes);
 
+//void addToList(State *st, string name, int &noStates, list<Status> &slist);
 #endif
