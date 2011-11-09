68d228e721eb178a507b914d6885a3ce812e85fc
Mario Stanke <mario.stanke@gmail.com>
2011-11-09 14:53:02 +0000

    Fixed bugs in maskNregions.pl: - last sequence was not processed at all - masked regions at the very beginning were not reported, when the sequence also ends in a masked region - masked regions of length 1 were not reported
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@67 946bced8-b911-4d30-a262-e0a769a8c5ad

--- scripts/maskNregions.pl
+++ scripts/maskNregions.pl
@@ -16,39 +16,48 @@ my $genome = $ARGV[0];
 my $fastaContent = "";
 my @seqLetters = ();
 
+sub procSeq;
+
 open (FASTA, $genome) or die ("\n\ncould not open file $genome!\n");
 LINE: while ($line = <FASTA>){
     next LINE if $line =~ m/^#/; #discard comments
     if ($line =~ /^>/){
-	   if(length($fastaContent)>1){
-		@seqLetters = split(//, $fastaContent);
-		$pos = 1;
-		foreach(@seqLetters){
-			if(($_=~m/n/) or ($_=~m/N/)){
-				if(not($seqLetters[$pos-2]=~m/n/) and not($seqLetters[$pos-2]=~m/N/)){
-					#print "$pos on $fastaHeader is the beginning of N-region\n";
-					print $fastaHeader."\t"."maskN"."\t"."N-region"."\t".$pos."\t";
-				}elsif(not($seqLetters[$pos]=~m/n/) and not($seqLetters[$pos]=~m/N/)){
-					#print "$pos on $fastaHeader is the end of N-region\n";
-					print $pos."\t"."."."\t"."."."\t"."."."\t"."N-region\n";
-				}
-			}
-			$pos = $pos+1;
-		}
-	   }
-           $line =~ s/[\x0A\x0D]+//g; #removing those ugly whitelines
-           $line =~ s/(\n)(\r)//g; #remove them alllll!
-           $line =~ m/(^>\w+)/i; #matches a word starting with > (Fasta)
-           $fastaHeader = substr($1, 1);
-	   $fastaContent = "";
+	procSeq($fastaContent);
+	$fastaContent = "";
     }else{
     $line =~ s/[\x0A\x0D]+//g; 
     $line =~ s/(\s+)(\n)(\r)//g;
     $fastaContent = $fastaContent.$line;
     }
 }
+procSeq($fastaContent) if ($fastaContent ne "");
+
 close(FASTA) or die "Could not close file $genome!\n";
 
-
+sub procSeq{
+    my $seq = shift; # nucleotide sequence
+    if(length($seq)>0){
+	@seqLetters = split(//, $seq);
+	$n = scalar(@seqLetters);
+	$pos = 1;
+	foreach(@seqLetters){
+	    if($_ eq "n" or $_ eq "N"){
+		if($pos == 1 || ($seqLetters[$pos-2] ne "n" and $seqLetters[$pos-2] ne "N")){
+		    #print "$pos on $fastaHeader is the beginning of N-region\n";
+		    print $fastaHeader."\t"."maskN"."\t"."N-region"."\t".$pos."\t";
+		}
+		if($pos == $n || ($seqLetters[$pos] ne "n" and $seqLetters[$pos] ne "N")){
+		    #print "$pos on $fastaHeader is the end of N-region\n";
+		    print $pos."\t"."."."\t"."."."\t"."."."\t"."N-region\n";
+		}
+	    }
+	    $pos = $pos+1;
+	}
+    }
+    $line =~ s/[\x0A\x0D]+//g; #removing those ugly whitelines
+    $line =~ s/(\n)(\r)//g; #remove them alllll!
+    $line =~ m/(^>\w+)/i; #matches a word starting with > (Fasta)
+    $fastaHeader = substr($1, 1);
+}
 
 
