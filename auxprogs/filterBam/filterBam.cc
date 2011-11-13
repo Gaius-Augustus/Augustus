2bbb1ebbce9bb2fa405ca88b187a81dbb5b10e65
Tonatiuh Pena Centeno <tonatiuh.pena.centeno@gmail.com>
2011-11-13 18:44:32 +0000

    Included a good part of the processQuery() function. Still a lot of testing and optimisation needs to be done. TPC 13.Nov.11
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@68 946bced8-b911-4d30-a262-e0a769a8c5ad

--- auxprogs/filterBam/filterBam.cc
+++ auxprogs/filterBam/filterBam.cc
@@ -1,7 +1,7 @@
 /*	Filters the coverage of a BAM alignment file
 
 	Created: 28-September-2011
-	Last modified: 5-November-2011
+	Last modified: 13-November-2011
 */ 
 
 #include <api/BamReader.h>
@@ -21,7 +21,7 @@
 #include <unordered_map>
 #include <list>
 #define QALISIZE 8
-#define LIMIT 8
+#define SIZE 3
 
 // Default options
 #define HELP  0
@@ -42,6 +42,7 @@
 
 using namespace BamTools;
 using namespace std;
+using namespace boost;
 
 // Definition of global variables
 static const char *optString = "abcdefghijklmnv:h?";
@@ -300,12 +301,12 @@ class Qali
   friend ostream &operator<<(ostream &, const Qali &);
 
    public:
-   	  string name[LIMIT];
+   	  string name[QALISIZE];
       Qali();
       Qali(const Qali &);
   	  Qali(string *message);
       ~Qali(){};
- 	  void push(string name[LIMIT]);
+ 	  void push(string name[QALISIZE]);
       Qali &operator=(const Qali &rhs);
       int operator==(const Qali &rhs) const;
       int operator<(const Qali &rhs) const;
@@ -315,7 +316,7 @@ class Qali
 // Constructor
 Qali::Qali()   
 {
-  for (int it=0; it<LIMIT; it++)
+  for (int it=0; it<QALISIZE; it++)
 	{
 	  name[it] = "";
 	}
@@ -324,7 +325,7 @@ Qali::Qali()
 // Constructor through string array
 Qali::Qali(string *message)
 {
-  for (int it=0; it<LIMIT; it++)
+  for (int it=0; it<QALISIZE; it++)
 	{
 	  name[it] = *(message+it);
 	}
@@ -334,16 +335,16 @@ Qali::Qali(string *message)
 // Copy constructor to handle pass by value.
 Qali::Qali(const Qali &copyin)   
 {           
-  for (int it=0; it<LIMIT; it++)
+  for (int it=0; it<QALISIZE; it++)
 	{
 	  this->name[it] = copyin.name[it];                
 	}
 }
 
 // push method
-void Qali::push(string name[LIMIT])
+void Qali::push(string name[QALISIZE])
 {
-  for (int it=0; it<LIMIT; it++)
+  for (int it=0; it<QALISIZE; it++)
 	{
   	this->name[it] = name[it];
 	}
@@ -352,7 +353,7 @@ void Qali::push(string name[LIMIT])
 // Ostream operator
 ostream &operator<<(ostream &output, const Qali &qali)
 {
-  for (int it=0; it<LIMIT; it++)
+  for (int it=0; it<QALISIZE; it++)
 	{
 	  output << qali.name[it] << ' ';
 	}
@@ -363,7 +364,7 @@ ostream &operator<<(ostream &output, const Qali &qali)
 // Assignment operator
 Qali& Qali::operator=(const Qali &rhs)
 {
-  for (int it=0; it<LIMIT; it++)
+  for (int it=0; it<QALISIZE; it++)
 	{
 	  this->name[it] = rhs.name[it];
 	}
@@ -374,7 +375,7 @@ Qali& Qali::operator=(const Qali &rhs)
 int Qali::operator==(const Qali &rhs) const
 {
   int it = 0;
-  while (it < LIMIT)
+  while (it < QALISIZE)
 	{
    	if( this->name[it] != rhs.name[it]) 
 	  {return 0;}
@@ -408,13 +409,129 @@ void printFormattedList(list<Qali> someList)
   for (it; it != someList.end(); it++)
 	{
 	  cout << "\t[ ";
-	  for (jit=0; jit<LIMIT; jit++)
+	  for (jit=0; jit<QALISIZE; jit++)
 		{
 		  cout << (*it).name[jit] << " ";
 		}
 	  cout << " ]," << endl;
 	}
 }
+
+
+
+
+// Class definition 
+class MatePairs
+{
+   friend ostream &operator<<(ostream &, const MatePairs &);
+
+   public:
+      float values[SIZE];
+      MatePairs();
+      MatePairs(const MatePairs &);
+      ~MatePairs(){};
+ 	  void push(float values[SIZE]);
+      MatePairs &operator=(const MatePairs &rhs);
+      int operator==(const MatePairs &rhs) const;
+      int operator<(const MatePairs &rhs) const;
+};
+
+// Constructor
+MatePairs::MatePairs()   // Constructor
+{
+  for (int it=0; it<SIZE; it++)
+	{
+	  values[it] = 0;
+	}
+}
+
+// Constructor through values
+MatePairs::MatePairs(const MatePairs &copyin)   // Copy constructor to handle pass by value.
+{                             
+	for (int it=0; it<SIZE; it++)
+		{
+	  	this->values[it] = copyin.values[it]; 	               
+		}  
+}
+
+// push method
+void MatePairs::push(float values[SIZE])
+{
+  for (int it=0; it<SIZE; it++)
+	{
+  	this->values[it] = values[it];
+	}
+}
+
+// Ostream operator
+ostream &operator<<(ostream &output, const MatePairs &aaa)
+{
+	for (int it=0; it<SIZE; it++)
+		{
+		  output << aaa.values[it] << ' ';
+		}
+  	output << endl;	
+   	return output;
+}
+
+// Assignment operator
+MatePairs& MatePairs::operator=(const MatePairs &rhs)
+{  
+	for (int it=0; it<SIZE; it++)
+	  {
+		this->values[it] = rhs.values[it];
+	  }
+   	return *this;
+}
+
+// Equality comparison operator
+int MatePairs::operator==(const MatePairs &rhs) const
+{
+  int it = 0;
+  while (it < SIZE)
+	{
+   	if( this->values[it] != rhs.values[it]) 
+	  {return 0;}
+	it++;
+	}	  
+}
+
+// This function is required for built-in STL list functions like sort
+int MatePairs::operator<(const MatePairs &rhs) const
+{
+   //if( this->values[1] == rhs.values[1] && this->values[4] <  rhs.values[4] ) return 1;
+   if( this->values[2] <  rhs.values[2] ) return 1;
+   return 0;
+}
+
+void printList(list<MatePairs> someList)
+{
+  list<MatePairs>::iterator it = someList.begin();
+  cout << " ";
+  for (it; it != someList.end(); it++)
+	{
+	  cout << *it << " ";
+	}
+}
+
+void printFormattedList(list<MatePairs> someList)
+{
+  list<MatePairs>::iterator it = someList.begin();
+  int jit;
+  for (it; it != someList.end(); it++)
+	{
+	  cout << "\t[ ";
+	  for (jit=0; jit<SIZE; jit++)
+		{
+		  cout << (*it).values[jit] << " ";
+		}
+	  cout << " ]," << endl;
+	}
+}
+
+
+float scoreMate(list<Qali>::iterator itt, list<Qali>::iterator jitt, float dist, bool best, int maxintronlen);
+void processQuery(list<Qali> qali, globalOptions_t globalOptions);
 
 
 int main(int argc, char *argv[])
@@ -456,7 +573,7 @@ int main(int argc, char *argv[])
   int outMinCover = 0;
   int line = 0;
   // Qali array
-  list<Qali> L; 
+  list<Qali> qali; 
   Qali QaliArray; 
   // Initialising options
   struct globalOptions_t globalOptions;
@@ -576,12 +693,12 @@ int main(int argc, char *argv[])
      	/* push @qali, [$_, $targetname, $qsuffix, $strand, $tstart, $tend, $percid, $coverage]; 
 		   Generating array of strings to do equivalent to: */
 		ss_rstart << rstart; ss_rend << rend; ss_percid << percid; ss_coverage << coverage;
-		string qali[QALISIZE] = {rName, qNameStem, qSuffix, (char*)strand, ss_rstart.str(), 
+		string qaliLine[QALISIZE] = {rName, qNameStem, qSuffix, (char*)strand, ss_rstart.str(), 
 								 ss_rend.str(), ss_percid.str(), ss_coverage.str()};
 		ss_rstart.str(""); ss_rend.str(""); ss_percid.str(""); ss_coverage.str(""); // clear strings
-   		QaliArray.push(qali);
-   		L.push_back(QaliArray); 
-		L.sort();
+   		QaliArray.push(qaliLine);
+   		qali.push_back(QaliArray); 
+		qali.sort();
 
 		/////////////////////////////////////////////////////////////////
 		//     Checking of file pairedness continues here 
@@ -602,6 +719,10 @@ int main(int argc, char *argv[])
 	} // end while	
   
 	/* 	Calling of: "processQuery() if ($qnamestem ne "");" goes here */
+	if (!qNameStem.compare(""))
+	  {
+		processQuery(qali, globalOptions);
+	  }
 
 
 	// Displaying results to STDOUT
@@ -632,3 +753,224 @@ int main(int argc, char *argv[])
 
 } // end main
 
+
+
+void processQuery(list<Qali> qali, globalOptions_t globalOptions)
+{
+  // Sorting by $tName and then by $tstart
+  qali.sort();
+
+  // Define matepairs
+  list<MatePairs> matepairs;
+  MatePairs mp;
+  list<int> insertlen;
+  map<int,int> mated;
+  list<Qali>::iterator itt;
+  list<Qali>::iterator jitt;
+  int32_t inslen, dist;
+  float scoremates, correction;
+  int it, jit;
+  float scoreArray[SIZE];
+  uint32_t jitTstart;
+  uint32_t jitTend;
+  uint32_t itTstart;
+  uint32_t itTend;
+  // Counters
+  int outPaired;
+  // Flags
+  bool uniq = globalOptions.uniq;
+  bool best = globalOptions.best;
+  bool verbose = globalOptions.verbose;
+  int uniqthresh = globalOptions.uniqthresh;
+  string pairbedfile = globalOptions.pairbedfile;
+  int maxintronlen = globalOptions.maxintronlen;
+
+  for (it=0, itt=qali.begin(); it<qali.size()-1; it++)
+	{
+	  for (jit=it+1, jitt=itt, jitt++; jit<qali.size(), (*itt).name[1]==(*jitt).name[1]; jit++) 
+		{
+		  cout << "comparing " << it << "," <<  (*itt).name[1] << ", with " << jit 
+			   	<< "," << (*jitt).name[1] << endl;
+		  // cout << (*itt).name[2] << ", " << (*jitt).name[2] << ", " << (*itt).name[3] << ", " 
+		  // 		<< (*jitt).name[3] << endl;
+		  	  if ((*itt).name[2]!=(*jitt).name[2])
+		  	  	{
+		  	  	  if ((*itt).name[3]!=(*jitt).name[3])
+		  	  	  	{
+					  jitTstart = lexical_cast<uint32_t> ((*jitt).name[4].c_str());
+		  			  jitTend = lexical_cast<uint32_t> ((*jitt).name[5].c_str());
+		  			  itTstart = lexical_cast<uint32_t> ((*itt).name[4].c_str());
+					  itTend = lexical_cast<uint32_t> ((*itt).name[5].c_str());
+		  	  		  dist = jitTstart - itTend - 1;
+		  	  		  if (itTstart>jitTstart)
+		  	  			{
+		  	  			  dist = itTstart - jitTend - 1;
+		  	  			}
+
+		  			  if (dist < maxintronlen && dist>=0)
+		  			  	{
+		  			  	  printf("found mate pair %d,%d\n", it, jit);
+						  ///////////////////////////////////////////////////////////////////////////////
+		  			  	  //push @matepairs, [$i,$j,scoreMate($i,$j,$dist)];
+		  				  scoreArray = {(float)it, (float)jit, scoreMate(itt, jitt, dist, best, maxintronlen)};
+		  				  mp.push(scoreArray); matepairs.push_back(mp);
+						  ///////////////////////////////////////////////////////////////////////////////
+		  				  if (!mated[it]) {mated[it]=0;}
+		  				  if (!mated[jit]) {mated[jit]=0;} 
+		  				  mated[it]++;
+		  				  mated[jit]++;
+		  				  inslen = jitTend - itTstart - 1;
+		  				  if (inslen<0)
+		  					{
+		  					  inslen = itTend - jitTstart - 1;
+		  					} 
+		  				  scoremates = scoreMate(itt, jitt, dist, best, maxintronlen);
+		  				  correction = dist/maxintronlen/10;
+	  				  	  // printf("mated[%d]=%d,mated[%d]=%d,inslen=%I32d,dist=%I32df,score=%2.5,
+						  // 		  correction=%2.5f\n", it, mated[it], jit, mated[jit], inslen, dist, 
+						  // 		  scoremates, correction);
+		  				  printf("mated[%d]=%d,mated[%d]=%d,inslen=%I32d,dist=%I32d\n", it, mated[it], 
+		  				  	  		jit, mated[jit], inslen, dist);
+						  ///////////////////////////////////////////////////////////////////////////////
+		  				  // push @insertlen, $inslen;
+		  				  insertlen.push_back(inslen); 
+						  ///////////////////////////////////////////////////////////////////////////////
+		  			  	} else {
+		  			  	  printf("distance not right\n");
+		  			  } // end inner if
+
+
+		  			} 
+		  		} // end outer if
+		  jitt++;
+		} // end inner for
+	  itt++;
+	} // end middle for
+
+
+  printf("found %d mate pairs, involving %d mates.\n", (int)matepairs.size(), (int)mated.size());
+  // $outPaired += @qali - scalar(keys %mated);
+  outPaired += qali.size() - mated.size();
+
+  if (!uniq && !best || matepairs.size()<2)
+	{ // let pass all read alignments that are involved in mate pairs
+	  list<Qali>::iterator l_it;
+	  map<int,int>::iterator m_it;
+	  for (m_it=mated.begin(), l_it = qali.begin(); m_it!=mated.end(); m_it++, l_it++)
+		{
+		  cout << (*l_it).name[0] << endl; 
+		}
+	} else {//uniq or best
+	  matepairs.sort();
+	  if (uniq)
+		{// let pass only best mate pair, and only if second is 
+		  // significantly worse
+		  int second = 1;
+		  list <MatePairs>::iterator l_second = matepairs.begin();
+		  while(second < matepairs.size()) // && similar())
+			{
+			  second++;
+			  l_second++;
+			}
+
+		  if (second < (int)matepairs.size())
+			  {
+				float ratio = (*l_second).values[2]/((*matepairs.begin()).values[2]);
+				if (verbose)
+				  {
+					printf("\nbest two mates\n");
+					// printf("" . join (", ", @{$qali[$matepairs[0]->[0]]}) . "\npaired with\n"
+					// .join (" ", @{$qali[$matepairs[0]->[1]]})
+					// 	   . "\nscore=$matepairs[0]->[2]\n");
+					// printf("" . join (", ", @{$qali[$matepairs[1]->[0]]}) . "\npaired with\n"
+					// .join (" ", @{$qali[$matepairs[1]->[1]]})
+					// 	   . "\nscore=$matepairs[1]->[2]\n");
+					printf("ratio = %2.3f\n", ratio);					
+				  } // end if(verbose)
+
+				if (ratio < uniqthresh)
+				  {
+					// // print the two alignments for best mate pair only
+					// printf("%d", matepairs); // $qali[$matepairs[0]->[0]]->[0]);
+					// printf("%d", matepairs); // $qali[$matepairs[0]->[1]]->[0]);
+				  } else {
+				  matepairs.clear();
+				  }
+			  } else {
+			  if (verbose)
+				{
+				  printf( "suboptimal mate pairs are similar\n");
+				}
+			  // printf($qali[$matepairs[0]->[0]]->[0]);
+			  // printf($qali[$matepairs[0]->[1]]->[0]);			
+			  }// end if(second<@matepairs.size())
+			///// splice @matepairs, 1; # keep only the best pair (if any)
+
+		} else { // best: take all best alignment pairs
+		int optscore;
+		list<string> bestTnames;
+		int numbest = 0;
+		while (numbest < matepairs.size()) 
+		  {
+			// printf($qali[$matepairs[$numbest]->[0]]->[0]);
+			// printf($qali[$matepairs[$numbest]->[1]]->[0]);
+			// push @bestTnames, $qali[$matepairs[$numbest]->[0]]->[1];
+			numbest++;
+		  }
+		// $outBest += @matepairs - $numbest;
+		// splice @matepairs, $numbest; # keep only the first $numbest pairs
+		if (bestTnames.size()>1)
+		  {
+			map<string,int> genenames; 
+			int itTname = 0; 
+            for (itTname; itTname<genenames.size(); itTname++) //my $Tname (@bestTnames) 
+				{ 
+				  // if (genenames[itTname]==1)
+				  // 	{
+				  // 	//$Tname =~ s/\.t\d+//; $genenames{$Tname}=1;
+				  // 	}
+				}
+			// print COMMON $oldqnamestem . "\t" . join(" ", keys %genenames) . "\n" if (%genenames > 1 && defined($commongenefile));
+
+		  }		
+ 	  } // end if(uniq)
+
+  } // end if (!uniq && !best || matepairs.size()<2)
+	
+  // output pairedbed info: go through list of all mate pairs and store start and end position
+  if (pairbedfile.length()>0)
+	{
+	  while (matepairs.size() > 0)
+		{
+	  	// DO SOMETHING ELSE
+		
+		}
+	}
+
+} // end processQuery()
+
+
+
+
+float scoreMate(list<Qali>::iterator itt, list<Qali>::iterator jitt, float dist, bool best, int maxintronlen)
+{
+  float score = (atof((*itt).name[6].c_str()) + atof((*jitt).name[6].c_str()))/100 // percent identity
+				+ (atof((*itt).name[7].c_str()) + atof((*jitt).name[7].c_str()))/100; // percent coverage
+  if (!best)
+  	{ // penalty for distance between mates. Do not use if option 'best' is chosen, 
+	  // otherwise a one base difference may cause a difference
+	  score -= dist/maxintronlen/10; 
+	}
+
+  return score;
+}
+
+//
+// checking whether two alignments (or two alignment pairs) are similar
+// Purpose: Due to separate handling of spliced and unspliced alignments it can happen
+// that very similar alignments are reported, e.g. an unspliced read going approximately up to an intron
+// and a spliced read with a few base pairs on one exon.
+// These should not be considered ambiguous when --uniq is specified.
+// bool similar()
+// {
+// }
