502b13da0dd3d3d8062ede99c6f42a7a69f0d328
Katharina Hoff <katharina.hoff@gmail.com>
2012-01-20 11:35:28 +0000

    script for making 'unique' lists of peptides with nonredundant multiplicity information
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@134 946bced8-b911-4d30-a262-e0a769a8c5ad
    
    Former-commit-id: 4f48e4dd6e0e8104e942dc230f6b161b57da921e

--- scripts/uniquePeptides.pl
+++ scripts/uniquePeptides.pl
@@ -1,24 +1,30 @@
 #!/usr/bin/perl
 
 # Creates a fasta file with unique peptides from a fasta input file
-# New fasta headers are random
-# Creates two output files: unique.fa header.mapping
+# where identical peptide match multiple positions. The first found 
+# peptide occurence is used for counting multiplicity.
+# Input format fasta header examples:
+# >scaffold4083_450 [15866 - 17011]  (65.81)
+# >au14.g511.t1 (46.17)
+# ... there is a score in the end of the header in round brackets!
+# Output format fasta header:
+# >peptideX mult=x
 # Do not use for long protein sequences, only for short peptides!!!
+# prefix
 
-my $usage = "uniquePeptides.pl in.fa unique.fa header.mapping\n";
+my $usage = "uniquePeptides.pl in.fa prefix > out.fa\n\nRead script head comments for futher documentation!\n\n";
 
-if(@ARGV!=3){print STDERR $usage; exit -1;}
+if(@ARGV!=2){print STDERR $usage; exit -1;}
 
 my $in = $ARGV[0];
-my $uniquefa = $ARGV[1];
-my $mapping = $ARGV[2];
+my $prefix = $ARGV[1];
 
-my %mapHash = ();
-my %seqHash = ();
+# two hashes for the same peptide (hash of hashes would be more elegant, but I am too lazy for that), key is always the peptide sequence, trimmed of "-" characters that sometimes appear to occur in Harald's peptide files.
+my %locHash = ();
+my %multHash = ();
 my $maxLen = 100;
 
 my $header;
-my $c = 1;
 
 open(IN, "<", $in) or die("Could not open file $in!\n");
 
@@ -26,38 +32,44 @@ while ( <IN> ) {
 	chomp;
 	if($_=~m/^>/){
 		$_=~s/>//;
+		$_=~s/\(\d+\.\d+\)//; # delete score
 		$header = $_;
 	}else{
 		if(length($_) > $maxLen){
 			print STDERR "The peptide $_ is longer $maxLen. Aborting.\n";
 			exit -1;
 		}
-		if(not(exists($seqHash{$_}))){
-			$seqHash{$_} = ">pep$c";
-			$mapKey = ">pep$c";
-			$c++;
-			
+		while($_=~m/-/){ # delete weird dashes in sequences
+			$_ = s/-//;
 		}
-		push (@{$mapHash{">pep$c"}}, $header);
+		if(not(exists($locHash{$_}))){
+			$locHash{$_} = "$header";
+			$multHash{$_} = 1;			
+		}elsif($locHash{$_} eq $header){
+			$multHash{$_} = $multHash{$_} + 1;
+		}
+		
+		
 	}
 }
 
 close IN or die("Could not close file $in!\n");
 
 # print unique fasta file
-open(OUT1, ">", $uniquefa) or die("Could not open file $uniquefa!\n");
-while ( ($k,$v) = each %seqHash ) {
-    print OUT1 "$v\n$k\n";
+my $c = 1;
+while ( ($k,$v) = each %multHash ) {
+	print ">$prefix$c mult=$v\n$k\n";
+	$c++;
 }
-close(OUT1) or die("Could not close file $uniquefa!\n");
+
 
 # print mapping file
-open(OUT2, ">", $mapping) or die("Could not close file $mapping!\n");
-for $k ( keys %mapHash ) {
-	print OUT2 "$k:\n";
-	foreach(@{$mapHash{$k}}){
-		print OUT2 "$_\n";
-	}
-}
-close(OUT2) or die("Could not close file $mapping!\n");
+#open(OUT2, ">", $mapping) or die("Could not close file $mapping!\n");
+#for $k ( keys %mapHash ) {
+#	print OUT2 "$k:\n";
+#	foreach(@{$mapHash{$k}}){
+#		print OUT2 "$_\n";
+#	}
+#}
+#close(OUT2) or die("Could not close file $mapping!\n");
 
