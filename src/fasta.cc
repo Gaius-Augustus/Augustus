33f001c0078fb1243e982731d1828d4f771e77f2
Mario Stanke <mario.stanke@gmail.com>
2012-09-02 14:36:24 +0000

    added src/fasta.cc
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@428 946bced8-b911-4d30-a262-e0a769a8c5ad
    
    Former-commit-id: b2b47bf4ec3aba7b8d6be1671eba8020d5d09729

---
+++ src/fasta.cc
@@ -0,0 +1,71 @@
+/**********************************************************************
+ * file:    fasta.cc
+ * licence: Artistic Licence, see file LICENCE.TXT or 
+ *          http://www.opensource.org/licenses/artistic-license.php
+ * descr.:  FASTA sequence input
+ * authors: Mario Stanke, mario.stanke@uni-greifswald.de
+ *
+ * date     |   author      |  changes
+ * ---------|---------------|------------------------------------------
+ * 01.09.12 | Mario Stanke  | creation of the file
+ **********************************************************************/
+
+// project includes
+#include "fasta.hh"
+
+// standard C/C++ includes
+#include <iostream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+/*
+ * Read the next sequence from input file stream and store the sequence,
+ * its name and length in the arguments. Memory for sequence and name are allocated here.
+ * 
+ */
+void readOneFastaSeq(ifstream &ifstrm, char* &sequence, char* &name, int &length){
+    string line;
+    string seq("");
+    char   c;
+    static int unnamedcount=1;
+    // skip empty lines
+    ifstrm >> ws;
+    if (!(ifstrm))
+	return;
+    c = ifstrm.peek();
+    if (c == '>') {// sequence name specified
+	getline(ifstrm, line);
+	// go up to the first white space, i.e. interpret only the first word as sequence identifier
+	int endpos = 1;
+	while (endpos < line.length() && !isspace(line[endpos]))
+	    endpos++;
+	endpos--;
+	name = new char[endpos+1];
+	strncpy(name, line.c_str()+1, endpos);
+	name[endpos] = '\0';
+    } else { // not correct fasta: unnamed sequence
+	name = new char[14]; // at most 100000 sequences
+	sprintf(name, "unnamed-%d", unnamedcount);
+    }
+    if (!ifstrm)
+	return;
+    while(ifstrm && ifstrm.peek( ) != '>'){
+	if (getline(ifstrm, line))
+	    seq.append(line);
+    }
+    sequence = new char[seq.length()+1];
+     
+    // now filter out any characters that are not letters
+    int pos = 0;
+    for (int i=0; i < seq.length(); i++) 
+	if (isalpha( seq[i] ))
+	    sequence[pos++] = tolower(seq[i]);
+    sequence[pos] = '\0';
+    length = pos;
+    if (length == 0){
+	delete sequence;
+	sequence = NULL;
+    }
+}
