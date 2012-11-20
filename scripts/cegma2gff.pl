898f6465336ce9760c189108575625dfea1d2c6f
Katharina Hoff <katharina.hoff@gmail.com>
2012-11-20 16:41:42 +0000

    script for converting CEGMA gff output format to a gff format that can be used by the AUGUSTUS web service.
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@474 946bced8-b911-4d30-a262-e0a769a8c5ad
    
    Former-commit-id: 55a632da832422809d79a6f689e8d93e8e779f4e

---
+++ scripts/cegma2gff.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+# Katharina J. Hoff, November 20th 2012
+#
+# Convert a cegma output.gff file to a gff file that is compatible with AUGUSTUS training
+#
+# Format example CEGMA:
+# CHROMOSOME_I    cegma   First   636237  636337  38.40   -       0       KOG0328.7
+# CHROMOSOME_I    cegma   Exon    636237  636337  38.40   -       .       KOG0328.7
+# CHROMOSOME_I    cegma   Internal        633894  634800  641.47  -       1       KOG0328.7
+# CHROMOSOME_I    cegma   Exon    633894  634800  641.47  -       .       KOG0328.7
+# CHROMOSOME_I    cegma   Internal        632564  632587  -9.15   -       0       KOG0328.7
+# CHROMOSOME_I    cegma   Exon    632564  632587  -9.15   -       .       KOG0328.7
+# CHROMOSOME_I    cegma   Terminal        631097  631288  138.28  -       0       KOG0328.7
+# CHROMOSOME_I    cegma   Exon    631097  631288  138.28  -       .       KOG0328.7
+#
+# Format example for AUGUSTUS:
+# CHROMOSOME_I    cegma   CDS    636237  636337  38.40   -       .       transcript_id "g1.KOG0328.7"
+# CHROMOSOME_I    cegma   CDS    633894  634800  641.47  -       .       transcript_id "g1.KOG0328.7"
+# CHROMOSOME_I    cegma   CDS    632564  632587  -9.15   -       .       transcript_id "g1.KOG0328.7"
+# CHROMOSOME_I    cegma   CDS    631097  631288  138.28  -       .       transcript_id "g1.KOG0328.7"
+
+my $usage = "cegma2gff.pl cegma.gff > augustus.gff\n";
+
+if(@ARGV!=1){print STDERR $usage; exit -1;}
+
+my $geneCounter = 0;
+my @t;
+
+open(CEGMA, "<", $ARGV[0]) or die("Could not open CEGMA file $ARGV[0]!\n");
+while(<CEGMA>){
+    if($_=~m/First/){$geneCounter = $geneCounter + 1;}
+    if($_=~m/Exon/){
+	chomp;
+	@t = split(/\t/);
+	print "$t[0]\t$t[1]\tCDS\t$t[3]\t$t[4]\t$t[5]\t$t[6]\t$t[7]\ttranscript_id \"g$geneCounter.$t[8]\"\n";
+    }
+}
+close(CEGMA) or die("Could not close CEGMA file $ARGV[0]!\n");
