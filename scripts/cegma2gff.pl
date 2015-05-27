a50ef5d737a8deeb552be51d7821c955ff9258bb
Katharina Hoff <katharina.hoff@gmail.com>
2015-05-27 08:03:37 +0000

    adapted cegma2gff.pl to cegma version 2.5 output format (script is still compatible with older versions of cegma)
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@991 946bced8-b911-4d30-a262-e0a769a8c5ad
    
    Former-commit-id: 49d6cdda06accee2a97a0c95a0f277f44b608d7d

--- scripts/cegma2gff.pl
+++ scripts/cegma2gff.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
 
-# Katharina J. Hoff, November 20th 2012
+# Katharina J. Hoff, May 26th 2015
 #
 # Convert a cegma output.gff file to a gff file that is compatible with AUGUSTUS training
 #
-# Format example CEGMA:
+# Format example CEGMA (v.2.4.010312):
 # CHROMOSOME_I    cegma   First   636237  636337  38.40   -       0       KOG0328.7
 # CHROMOSOME_I    cegma   Exon    636237  636337  38.40   -       .       KOG0328.7
 # CHROMOSOME_I    cegma   Internal        633894  634800  641.47  -       1       KOG0328.7
@@ -14,6 +14,12 @@
 # CHROMOSOME_I    cegma   Terminal        631097  631288  138.28  -       0       KOG0328.7
 # CHROMOSOME_I    cegma   Exon    631097  631288  138.28  -       .       KOG0328.7
 #
+# Format example CEGMA (v.2.5):
+# CHROMOSOME_I    cegma   First    636237  636337  38.40   -       .       KOG0328.7
+# CHROMOSOME_I    cegma   Internal    633894  634800  641.47  -       .       KOG0328.7
+# CHROMOSOME_I    cegma   Internal    632564  632587  -9.15   -       .       KOG0328.7
+# CHROMOSOME_I    cegma   Terminal    631097  631288  138.28  -       .       KOG0328.7  
+
 # Format example for AUGUSTUS:
 # CHROMOSOME_I    cegma   CDS    636237  636337  38.40   -       .       transcript_id "g1.KOG0328.7"
 # CHROMOSOME_I    cegma   CDS    633894  634800  641.47  -       .       transcript_id "g1.KOG0328.7"
@@ -30,7 +36,7 @@ my @t;
 open(CEGMA, "<", $ARGV[0]) or die("Could not open CEGMA file $ARGV[0]!\n");
 while(<CEGMA>){
     if(($_=~m/First/) or ($_=~m/Single/)){$geneCounter = $geneCounter + 1;}
-    if($_=~m/Exon/){
+    if(not($_=~m/Exon/)){
 	chomp;
 	@t = split(/\t/);
 	print "$t[0]\t$t[1]\tCDS\t$t[3]\t$t[4]\t$t[5]\t$t[6]\t$t[7]\ttranscript_id \"g$geneCounter.$t[8]\"\n";
