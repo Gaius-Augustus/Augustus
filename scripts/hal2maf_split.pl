6c5de9debddf65cf44e971300a6427e625ecbc9a
Stefanie Nachtweide <stefanie.nachtweide@gmail.com>
2015-07-14 14:46:24 +0000

    hal2maf_split.pl: changed the splitting point from a fixed 500bp upstream/donwstream of a genic region, to a variable number of bps depending on the size of the intergenic region upstream/downstream.
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@1027 946bced8-b911-4d30-a262-e0a769a8c5ad
    
    Former-commit-id: 91d2962442432263237cf27e13ccbcffb104011b

--- scripts/hal2maf_split.pl
+++ scripts/hal2maf_split.pl
@@ -63,9 +63,8 @@ my $h2m_param = "";
 my $hal_exec_dir;
 my $chunksize = 2500000;
 my $overlap = 500000;
-my $padding = 500;         # must be smaller than min_intergenic, splitting point is this number of bases
-                           # upstream/downstream of a genic interval
-my $min_intergenic = 1000; # when a list of genic intervals is given,
+my $padding = 10000; 
+my $min_intergenic = 2000; # when a list of genic intervals is given,
                            # all intervals that are within this distance are combined to a single interval
 my $cpus = 1;
 
@@ -167,6 +166,7 @@ if(defined($no_split_list)){
     while(<IN>){
 	chomp;
 	my($chr,$start,$end)=split("\t",$_);
+	next if(defined($refSequence) && $refSequence ne $chr); # if a reference sequence is specified, skip all others
 	push @intervals,[$chr, $start, $end];
     }
     close(IN);
@@ -176,8 +176,9 @@ if(defined($no_split_list)){
 # sort intervals by 1. chromosome, 2. start
 @intervals = sort {$a->[0] cmp $b->[0] || $a->[1] <=> $b->[1]} @intervals;
 
-my @joined=(); # array of sorted, joined genic intervals
+my @joined=(); # array of sorted, joined genic intervals: Elements: <seq> <start> <end> <end of preceeding interval> <start of succeeding interval>
 my ($chr, $start, $end);
+my $pred_end = 0;
 
 foreach (@intervals){
     if(defined($chr) && $_->[0] eq $chr && $_->[1] - $end <= $min_intergenic ) {
@@ -186,14 +187,48 @@ foreach (@intervals){
         }
     } else {
         if (defined($chr)){
-            push @joined,[$chr, $start, $end];
+	    my $succ_start = $seqHash{$chr} - 1; # start position of succeeding interval on this sequence
+	    if($_->[0] eq $chr){ 
+		$succ_start = $_->[1];
+	    }
+            push @joined,[$chr, $start, $end, $pred_end, $succ_start];
+	    $pred_end = $end;  # end position of preceeding interval on this sequence
+	    if($_->[0] ne $chr){
+		$pred_end = 0;
+	    }
         }
         ($chr, $start, $end) = ($_->[0], $_->[1], $_->[2]);
     }
 }
-push @joined,[$chr, $start, $end];
+my $succ_start = $seqHash{$chr} - 1;
+push @joined,[$chr, $start, $end, $pred_end, $succ_start];
+
 
 # calculate start and end positions of alignment chunks
+#
+# if a list of genic intervals is specified, the start/end of an alignment chunk is shifted to
+# an intergenic region if it is contained in a genic interval:
+#
+# case 1)
+# the start position s of an alignment chunk A is contained in a genic interval 
+#
+#                                                                 s
+#                                                                 |----------------------|  alignment chunk A
+#                               |---------|                  |---------|    genic intervals I1,...,Im (all disjunct) 
+#                                         y                  x
+#
+# => s is shifted to the upstream intergenic region: s=max{(x+y)/2, x-10kb}
+#
+# case 2)
+# the end position e of an alignment chunk A is contained in a genic interval 
+#
+#                                    e
+#             |----------------------|  alignment chunk A
+#                               |---------|                  |---------|    genic intervals I1,...,Im (all disjunct) 
+#                                         x                 y
+#
+# => e is shifted to the downstream intergenic region: e=min{(x+y)/2, x+10kb}
+
 my @aln_chunks = (); # each element is one alignment chunk, e.g. sequence segment (seqname:start-end) in the reference
 
 foreach my $seq (sort {$a cmp $b} keys %seqHash ){
@@ -208,14 +243,20 @@ foreach my $seq (sort {$a cmp $b} keys %seqHash ){
 	foreach(@joined) {
 	    last if($_->[0] gt $seq || ($_->[0] eq $seq && $_->[1] > $start));
 	    if($_->[0] eq $seq && $_->[1] <= $start && $_->[2] >= $start){ # start position of alignment chunk within a genic interval
-		$start=$_->[1]-$padding; # shift start to $padding bases upstream of genic interval
+		$start = int(($_->[1]+$_->[3])/2); #shift start position fo upstream intergenic region
+		if($_->[1]-$padding > $start){
+		    $start=$_->[1]-$padding;
+		}
 		last;
 	    }
 	}
 	foreach(@joined) {
 	    last if($_->[0] gt $seq || ($_->[0] eq $seq && $_->[1] > $end));
 	    if($_->[0] eq $seq && $_->[1] <= $end && $_->[2] >= $end){ # end position of alignment chunk within a genic interval
-		$end=$_->[2]+$padding; # shift end to $padding bases downstream of genic interval 
+		$end = int(($_->[2]+$_->[4])/2); # shift end position to downstream intergenic region
+		if($_->[2]+$padding<$end){
+		    $end=$_->[2]+$padding;
+		}
 		last;
 	    }
 	}
@@ -227,7 +268,10 @@ foreach my $seq (sort {$a cmp $b} keys %seqHash ){
     foreach(@joined) {
 	last if($_->[0] gt $seq || ($_->[0] eq $seq && $_->[1] > $start));
 	if($_->[0] eq $seq && $_->[1] <= $start && $_->[2] >= $start){
-	    $start=$_->[1]-$padding;
+	    $start = int(($_->[1]+$_->[3])/2); 
+	    if($_->[1]-$padding > $start){
+		$start=$_->[1]-$padding;
+	    }
 	    last;
 	}
     }
