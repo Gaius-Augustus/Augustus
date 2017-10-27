cf2a22d0084e9b31cb6e98ba90af6a401a266bd6
Katharina Hoff <katharina.hoff@gmail.com>
2017-10-27 10:51:55 +0000

    added script to fix WebAUGUSTUS gff format check speed
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@1319 946bced8-b911-4d30-a262-e0a769a8c5ad
    
    Former-commit-id: d4b4ea2b08dd1bc947c54855e8abf5b6e31bbc91

---
+++ scripts/findGffNamesInFasta.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+# Author: Katharina J. Hoff
+# Date: Oct. 27 2017
+
+# This script is part of WebAUGUSTUS
+# It check whether scaffold names in a given gff file are present in a related genome fasta file as headers
+
+use strict;
+use Getopt::Long;
+
+my $usage = "$0 -- check whether scaffold names in a given gff file are present in a related genome fasta file as headers\n\n";
+$usage .= "Usage: $0 --gff=gffFile --genome=genomeFile --out=outFile\n";
+$usage .= "An integer 1 will be casted to STDOUT if the gff file contains scaffold names that are NOT present in the genome file, otherwise 0 will be casted.\nAnother integer (0/1) will be cast to outFile concerning the number of columns; if 9, then 0; if different from 9, then 1.";
+
+my $gffFile;
+my $genomeFile;
+my $outFile;
+
+GetOptions('gff=s'=>\$gffFile,
+           'genome=s'=>\$genomeFile,
+           'out=s'=>\$outFile);
+
+my %headers;
+open(GENOME, "<", $genomeFile) or die("Could not open file $genomeFile!\n");
+while(<GENOME>){
+    if(m/^>/){
+	chomp;
+	$_ =~ s/>//;
+	$headers{$_} = 1;
+    }
+}
+close(GENOME) or die("Could not close file $genomeFile!\n");
+
+my @line;
+my $error1 = 0;
+my $error2 = 0;
+my $nCols;
+open(GFF, "<", $gffFile) or die("Could not open file $gffFile!\n");
+while(<GFF>){
+    @line = split(/\t/);
+    if(not(defined($headers{$line[0]}))){
+	$error1 = 1;
+    }
+    $nCols = @line;
+    if(not($nCols==9) && not($nCols==0)){
+	$error2 = 1;
+    }
+
+    
+    
+}
+close(GFF) or die("Could not close file $gffFile!\n");
+
+print STDOUT "$error1";
+
+open(OUT, ">", $outFile) or die ("Could not open file $outFile!\n");
+print OUT $error2;
+close(OUT) or die ("Could not close file $outFile!\n");
