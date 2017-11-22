bc8ed610e8e4e639f7805a0ac9bb25888288dbbd
Katharina Hoff <katharina.hoff@gmail.com>
2017-11-22 13:10:50 +0000

    added script for modification of stop codon freqs
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@1325 946bced8-b911-4d30-a262-e0a769a8c5ad
    
    Former-commit-id: aee82a9385aa85523e5e3bbc98bdd26ae2d75975

---
+++ scripts/setStopCodonFreqs.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl
+# Author: Katharina J. Hoff
+# Date: November 22nd 2017
+# set stop codon frequencies in parameters.cfg of a particular species
+
+use strict;
+use Getopt::Long;
+
+my $usage = "filterSpliceHints.pl --AUGUSTUS_CONFIG_PATH=path --species=someName --etrainOut=file\n";
+
+my $AUGUSTUS_CONFIG_PATH;
+my $species;
+my $etrainOut;
+my $help;
+
+GetOptions( 'AUGUSTUS_CONFIG_PATH=s'  => \$AUGUSTUS_CONFIG_PATH,
+	    'species=s'              => \$species,
+	    'etrainOut'              => \$etrainOut,
+	    'help!'                  => \$help);
+	    
+if($help){
+    print $usage;
+    exit 0;
+}
+
+if(not(defined($AUGUSTUS_CONFIG_PATH))){
+    if(-e $ENV{'AUGUSTUS_CONFIG_PATH'}){
+	$AUGUSTUS_CONFIG_PATH=$ENV{'AUGUSTUS_CONFIG_PATH'};
+    }else{
+	print $usage;
+	die("$AUGUSTUS_CONFIG_PATH missing!\n");	
+    }		  
+}
+
+if(not(defined($species))){
+    print $usage;
+    die("No species defined!\n");
+}
+
+if(not(defined($etrainOut))){
+    print $usage;
+    die("No etraining output file defined!\n");
+}
+
+my $freqOfTag;
+my $freqOfTaa;
+my $freqOfTga;
+open(ETRAIN, "<", $etrainOut) or die ("Could not open file $etrainOut!\n");
+while(<ETRAIN>){
+    if(/tag:\s*.*\((.*)\)/){     
+	$freqOfTag = $1;         
+    }elsif(/taa:\s*.*\((.*)\)/){ 
+	$freqOfTaa = $1;         
+    }elsif(/tga:\s*.*\((.*)\)/){ 
+	$freqOfTga = $1;         
+    }
+}
+close(ETRAIN) or die ("Could not close file $etrainOut!\n");
+
+my $cfgFile = "$AUGUSTUS_CONFIG_PATH/species/$species/$species\_parameters.cfg";
+if(-e "$cfgFile"){
+    setParInConfig($cfgFile, "/Constant/amberprob", $freqOfTag);
+    setParInConfig($cfgFile, "/Constant/ochreprob", $freqOfTaa);
+    setParInConfig($cfgFile, "/Constant/opalprob", $freqOfTga);
+}else{
+    die("Species config file $cfgFile does not seem to exist!\n");
+}
+
+sub setParInConfig{
+    my $configFileName = shift;
+    my $parName = shift;
+    my $value = shift;
+    open(CFGFILE, "+<$configFileName") or die ("Could not read config file $configFileName\n");
+    my @lines = <CFGFILE>;
+    foreach my $line (@lines){
+	$line =~ s/(\s*$parName +)(\S+?)(\s|\#|$)/$1$value$3/;
+    }
+    seek(CFGFILE, 0,0);
+    print CFGFILE @lines or die ("Could not write $configFileName");
+    truncate(CFGFILE, tell(CFGFILE));
+    close(CFGFILE);
+}
