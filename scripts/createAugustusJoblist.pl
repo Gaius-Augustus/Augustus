00a6a23c63d5cec4215041cb95a25dae363ae39b
Katharina Hoff <katharina.hoff@gmail.com>
2018-02-15 10:04:51 +0000

    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@1343 946bced8-b911-4d30-a262-e0a769a8c5ad
    Former-commit-id: 294f311dfc1ca0859c59a4bdd02d7de3937da9ca

--- scripts/createAugustusJoblist.pl
+++ scripts/createAugustusJoblist.pl
@@ -129,6 +129,7 @@ while (<SEQ>) {
         $wholecommand = "";
         if( $partitionHints ) {
             $localHints = "$outputdir/$seqnr.$chunkid.${name}.$predStart..$pE.hints";
+            print ${name}."\n";
             my $locus = ${name} =~ s/(.*)\.*/$1/;
             $wholecommand .= "grep ^$locus $hints | awk ' {if (\$4 >= $predStart ) print \$0 } ' | awk ' {if (\$5 <= $pE) print \$0 } ' > $localHints\n";
         } else {
