3977ed23c32f930a3c5e161f9bdbe421b36f723c
Katharina Hoff <katharina.hoff@gmail.com>
2018-02-16 06:41:24 +0000

    migrated eval_multi_gtf.pl from ag
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@1346 946bced8-b911-4d30-a262-e0a769a8c5ad
    
    Former-commit-id: 2b202a21c37708184127b45c2247318e8cd90a82

---
+++ scripts/eval_multi_gtf.pl
@@ -0,0 +1,112 @@
+#!/usr/bin/perl
+#
+# compute the accuracy values of a set of predictions
+# against a set of annotations
+#
+#
+#
+# Katharina Hoff & Mario Stanke, February 16th 2018
+
+use strict;
+use warnings;
+use File::Path 'rmtree';
+use Cwd 'abs_path';
+use File::Basename;
+use Getopt::Long;
+use FindBin;
+use File::Which qw(which where);    # exports which() and where()
+
+my $usage = "$0 -- compute the prediction accuracy\n";
+$usage .= "Usage: $0 seqlist annotation.gtf prediction.gtf\n";
+$usage .= "Options:\n";
+$usage
+    .= "--EVAL_PATH   path to eval package (will try to guess from "
+    . "availability of evaluate_gtf.pl, but if that does not work because you "
+    . "don't have this script in your path, set with this command line option.\n";
+
+if ( $#ARGV != 2 ) {
+    die "Unknown option\n\n$usage";
+}
+my $seqlistfilename = $ARGV[0];
+my $annofilename    = $ARGV[1];
+my $predfilename    = $ARGV[2];
+my $help;
+my $evalpath;
+GetOptions(
+    'EVAL_PATH=s' => \$evalpath,
+    'help!'       => \$help
+);
+
+if ($help) { print $usage; }
+
+if ($evalpath) {
+    my $last_char = substr( $evalpath, -1 );
+    if ( $last_char eq "\/" ) {
+        chop($evalpath);
+    }
+}
+else {
+    my $epath = which 'evaluate_gtf.pl';
+    $evalpath = dirname( abs_path($epath) );
+}
+
+if ( not( -e "$evalpath/evaluate_gtf.pl" ) ) {
+    die("Cannot find $evalpath/evaluate_gtf.pl");
+}
+
+open( SEQLIST, "<$seqlistfilename" )
+    or die("Could not open $seqlistfilename");
+
+################################################################################
+#
+# create for each sequence in seqlist
+# a gtf file with annotation and a gtf file with prediction
+#
+################################################################################
+# create a temporary directory
+my $dirname = "tempgtf";
+system("rm -rf $dirname; mkdir $dirname");
+
+# create the two list files of gtffiles
+system("rm -f annotation_list");
+system("rm -f prediction_list");
+
+my @seqlist = <SEQLIST>;
+
+close(SEQLIST) or die("Could not close $seqlistfilename");
+
+foreach my $seq (@seqlist) {
+    chomp $seq;
+    my $cmdStr
+        = "grep \"^$seq\\b\" $annofilename | grep -P \"\\t\\+\\t\" > "
+        . "$dirname/$seq"
+        . "_plus.anno.gtf";
+    system($cmdStr);
+    $cmdStr
+        = "grep \"^$seq\\b\" $predfilename | grep -P \"\\t\\+\\t\" > "
+        . "$dirname/$seq"
+        . "_plus.pred.gtf";
+    system($cmdStr);
+    system( "echo '$dirname/$seq" . "_plus.anno.gtf' >> annotation_list" );
+    system( "echo '$dirname/$seq" . "_plus.pred.gtf' >> prediction_list" );
+    system(   "grep \"^$seq\\b\" $annofilename | grep -P \"\\t-\\t\" > "
+            . "$dirname/$seq"
+            . "_minus.anno.gtf" );
+    system(   "grep \"^$seq\\b\" $predfilename | grep -P \"\\t-\\t\"> "
+            . "$dirname/$seq"
+            . "_minus.pred.gtf" );
+    system( "echo '$dirname/$seq" . "_minus.anno.gtf' >> annotation_list" );
+    system( "echo '$dirname/$seq" . "_minus.pred.gtf' >> prediction_list" );
+
+}
+
+# call evaluate_gtf
+#
+system(
+    "perl -I $evalpath $evalpath/evaluate_gtf.pl annotation_list prediction_list"
+);
+
+# clean up
+unlink("annotation_list");
+unlink("prediction_list");
+rmtree( ["$dirname"] );
