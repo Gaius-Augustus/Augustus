644bf1273bf3065561b49fae9569a0ae68599922
Katharina Hoff <katharina.hoff@gmail.com>
2018-03-01 10:11:31 +0000

    adaptation of join_mult_hints.pl to join only hints from the same src
    git-svn-id: file:///home/gumz3/Desktop/svnAugustusRepo/svnArchiveFile/svn/augustus/trunks@1416 946bced8-b911-4d30-a262-e0a769a8c5ad
    
    Former-commit-id: e8466f4f2ad0c21e68f068bd99ee80f25f94af2d

--- scripts/join_mult_hints.pl
+++ scripts/join_mult_hints.pl
@@ -7,20 +7,21 @@
 # pipeline may fail upon custom modification of this script.
 # In case of doubt, contact katharina.hoff@uni-greifswald.de
 #
-# Mario Stanke, 4.1.2010, last modification by Katharina J. Hoff on Feb 21st 2018
+# Mario Stanke, 4.1.2010, last modification by Katharina J. Hoff on March 1st 2018
 
 use strict;
 use Getopt::Long;
 
-my $usage = "$0 -- summarize multiple identical hints to one with mult=n\n";
-$usage .= "\n";
-$usage .= "Usage: $0 <in.hints >joined.hints\n";
-$usage .= "  PREREQUISITE: input GFF file must be sorted so that hints that should be summarized are below each other\n";
-$usage .= "  e.g. do a cat hints.gff | sort -n -k 4,4 | sort -s -n -k 5,5 | sort -s -k 3,3 | sort -s -k 1,1 | join_mult_hints.pl\n";
+my $usage = "$0 -- summarize multiple identical hints to one with mult=n\n"
+	 	. "\n"
+		. "Usage: $0 <in.hints >joined.hints\n"
+		. "  PREREQUISITE: input GFF file must be sorted so that hints that should be summarized are below each other\n"
+		. "  e.g. do a cat hints.gff | sort -n -k 4,4 | sort -s -n -k 5,5 | sort -s -k 3,3 | sort -s -k 1,1 | join_mult_hints.pl\n"
+		. "  hints are only joined if they are from the same src (e.g. src=E)\n"
+		. "  score in column 6 is adapted to mult= value for usage with GeneMark\n";
 
-
-my $help=0;
-GetOptions('help!'=>\$help);
+my $help = 0;
+GetOptions( 'help!' => \$help );
 if ($help) {
     print "$usage";
     exit(0);
@@ -28,30 +29,67 @@ if ($help) {
 
 my @f;
 my @lf;
-my ($lm,$m);
+my ( $lm, $m );
+my %identical; # holds all hints with identical coordinates, locus, strand & frame
 
-while (<>) {
-    @f = split(/\t/,$_);
-    if (!(@lf)){
-	@lf = @f;
-    } elsif (!(($f[0] eq $lf[0]) && ($f[2] eq $lf[2]) && ($f[3] == $lf[3]) && ($f[4] == $lf[4])  && ($f[6] eq $lf[6]) && ($f[7] eq $lf[7]))){
-	print join("\t",@lf);
-	@lf = @f;
-    } else {
-	# update lf by adding f to it
-	$lf[8] =~ s/gro?u?p=[^;]*;//;
-	if ($lf[8] =~ /mult=(\d+);/){
-	    $lm = $1;
-	    $lf[8] =~ s/mult=\d+;//;
-	} else {
-	    $lm = 1;
-	}
-	if ($f[8] =~ /mult=(\d+);/){
-	    $m = $1;
-	} else {
-	    $m = 1;
-	}
-	$lf[8] = "mult=" . ($lm+$m) . ";" . $lf[8];
+while ( <STDIN> ) {
+    @f = split( /\t/, $_ );
+
+    if ( !(@lf) ) {
+        @lf = @f;
+    }
+    elsif (
+        !(     ( $f[0] eq $lf[0] )
+            && ( $f[2] eq $lf[2] )
+            && ( $f[3] == $lf[3] )
+            && ( $f[4] == $lf[4] )
+            && ( $f[6] eq $lf[6] )
+            && ( $f[7] eq $lf[7] )
+        )
+        )
+    {
+        summarizeHint(\%identical);
+        undef %identical;
+        @lf = @f;
+    }
+    else {
+    	if( $f[9] =~ m/s?o?urc?e=(\w)/ ) {
+    		push(@{$identical{$1}}, @f);
+    	}else{
+    		push(@{$identical{'no_src'}}, @f);
+    	}
     }
 }
-print join("\t",@lf) if (@lf);
+print join( "\t", @lf ) if (@lf);
+
+sub summarizeHint {
+	my $hints = shift;
+	foreach my $src (keys %{$hints}) {
+		my @h = split(/\t/, ${$hints->{$src}}[0]);
+		# if there is only one hint, print exactly as it was
+		if(scalar(@{$hints->{$src}}) == 1 ) {
+			print join ("\t", @h);
+		}else{
+			my $mult = 1;
+			for (my $i = 1; $i < scalar (@{$hints->{$src}}); $i++ ) {
+				my @l = split(/\t/, ${$hints->{$src}}[$i]);
+				if($l[9] =~ m/mult=(\d+)/) {
+					$mult += $1;
+				}else{
+					$mult++;
+				}
+			}
+			$h[8] =~ s/gro?u?p=[^;]*;//;
+			$h[8] =~ s/mult=\d?;//;
+			for (my $i = 0; $i < 9; $i++) {
+				if( not($i==5) && not ($i==8)) {
+					print $h[$i]."\t";
+				}elsif($i==5){
+					print $mult."\t";
+				}elsif($i==8){
+					print "mult=$mult;$h[$i]";
+				}
+			}
+		}
+	}
+}
