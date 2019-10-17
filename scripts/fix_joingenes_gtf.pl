e25f8ed15e25f6372d6cf0d41ffd0552473be7f7
Katharina Hoff <katharina.hoff@gmail.com>
2019-10-17 12:59:35 +0200

    fix_joingenes_gtf.pl : printing intron features as suggested in BRAKER issue https://github.com/Gaius-Augustus/BRAKER/issues/88

--- scripts/fix_joingenes_gtf.pl
+++ scripts/fix_joingenes_gtf.pl
@@ -10,6 +10,8 @@
 #                                                                                                  #
 # Contact: katharina.hoff@uni-greifswald.de                                                        #
 #                                                                                                  #
+# Acknowledgements: I thank Martijn Holterman for suggesting to print introns in the output file.  #
+#                                                                                                  #
 # This script is under the Artistic Licence                                                        #
 # (http://www.opensource.org/licenses/artistic-license.php)                                        #
 ####################################################################################################
@@ -46,9 +48,11 @@ if ($help) {
 my %genes;
 my $comments = "";
 my $tx_id;
+my @previous;
 while(<STDIN>){
 	if(m/^\#/){
 		$comments .= $_;
+		@previous = ();
 	}else{
 		chomp;
 		my @t = split(/\t/);
@@ -77,9 +81,18 @@ while(<STDIN>){
 				$genes{$tx_id}{'stop'} = $t[4];
 			}
 			push(@{$genes{$tx_id}{'lines'}}, $t[0]."\t".$t[1]."\t".$t[2]."\t".$t[3]."\t".$t[4]."\t".$t[5]."\t".$t[6]."\t".$t[7]."\ttranscript_id \"".$t[8]."\"; gene_id \"".$tx_id."\"");
+		}elsif(m/\tCDS\t/){
+			if ($previous[2] eq "CDS"){
+				my $start = $previous[4] + 1;
+				my $end = $t[3] - 1;
+				push(@{$genes{$tx_id}{'lines'}}, $t[0]."\t".$t[1]."\tintron\t".$start."\t".$end."\t.\t".$t[6]."\t.\t".$t[8]);
+			}
+			push(@{$genes{$tx_id}{'lines'}}, $_);
+			push(@{$genes{$tx_id}{'lines'}}, $t[0]."\t".$t[1]."\texon\t".$t[3]."\t".$t[4]."\t.\t".$t[6]."\t.\t".$t[8]);
 		}else{
 			push(@{$genes{$tx_id}{'lines'}}, $_);
 		}
+		@previous = @t;
 	}
 }
 
