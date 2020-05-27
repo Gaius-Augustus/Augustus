6233a411f6d8b7f726b712e8d8bd5cc163cc09a5
mglgnn <g.migliorelli@gmail.com>
2020-05-27 15:35:39 +0200

    code added to prepare small data set and run prediction on it

--- include/compgenepred.hh
+++ include/compgenepred.hh
@@ -13,6 +13,15 @@
 #include "randseqaccess.hh"
 #include "phylotree.hh"
 
+
+#ifdef TESTING
+void mergeInterval(list<tuple<string,int,int> >& interval);
+void mergeIntervals(vector<string>& speciesNames, vector<list<tuple<string,int,int> > >& intervals);
+bool sortInterval(const tuple<string,int,int>& a, const tuple<string,int,int>& b);
+void writeIntervals(string dirname, vector<string>& speciesNames, vector<list<tuple<string,int,int> > >& intervals);
+bool shiftGFF(string filename);
+#endif
+
 /**
  * @brief comparative gene prediction on multiple species
  * 
@@ -24,6 +33,14 @@ class CompGenePred {
 public:
     CompGenePred();
     ~CompGenePred() { delete rsa;}
+
+    #ifdef TESTING
+    // helpers for testing
+    void prepareTest();   
+    void runTest();
+    void postprocTest();
+    bool readInterval(string filename, list<tuple<string,int,int> >& grlist);
+    #endif
 
     void start();
     RandSeqAccess *rsa;
