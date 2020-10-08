1cc29b649432b9952b87d6e470ac160ab6901316
Daniel Honsel <dhonsel@informatik.uni-goettingen.de>
2020-10-08 10:43:51 +0200

    clean up

--- tests/longrunning/write_to_influx.py
+++ tests/longrunning/write_to_influx.py
@@ -21,7 +21,7 @@ parser.add_argument('-m', '--mode', help='the testcase mode: single or cgp.')
 args = parser.parse_args()
 
 
-def write_single(datadir, drop=False):
+def write(datadir, mode, drop=False):
     client = InfluxDBClient(host=args.dbhost, port=args.dbport,
                             username=args.dbuser, password=args.dbpasswd, database=args.dbname)
 
@@ -31,7 +31,11 @@ def write_single(datadir, drop=False):
     with open(datadir + 'test_version_data.json') as json_file:
         version_data = json.load(json_file)
 
-    eval_dir = datadir + 'eval/'
+    if mode == 'single':
+        eval_dir = datadir + 'eval/'
+    else:
+        eval_dir = datadir + 'ACCURACY/'
+
     for metric_file in glob.glob(eval_dir + '*.json'):
         with open(metric_file) as json_file:
             metric_data = json.load(json_file)
@@ -39,14 +43,19 @@ def write_single(datadir, drop=False):
             # do not save the execution time as tag
             metric_data['execution_time'] = version_data['execution_time']
             
+            if mode == 'single':
+                testcase = os.path.basename(metric_file).replace('.json', '')
+            else:
+                testcase = 'default'
+
             json_body = [
                 {
                     "measurement": "qualitymetrics",
                     "tags": {
-                        'mode': 'single',
+                        'mode': mode,
                         "hash": version_data['revision'],
                         'softmasking': version_data['softmasking'],
-                        'testcase': os.path.basename(metric_file).replace('.json', '')
+                        'testcase': testcase
                     },
                     "time": version_data['commit_date'],
                     "fields": metric_data
@@ -54,43 +63,6 @@ def write_single(datadir, drop=False):
             ]
 
             client.write_points(json_body)
-            # print(json_body)
-
-
-def write_cgp(datadir, drop=False):
-    client = InfluxDBClient(host=args.dbhost, port=args.dbport,
-                            username=args.dbuser, password=args.dbpasswd, database=args.dbname)
-
-    if drop:
-        client.drop_measurement('qualitymetrics')
-
-    with open(datadir + 'test_version_data.json') as json_file:
-        version_data = json.load(json_file)
-
-    eval_dir = datadir + 'ACCURACY/'
-    for metric_file in glob.glob(eval_dir + '*.json'):
-        with open(metric_file) as json_file:
-            metric_data = json.load(json_file)
-
-            # do not save the execution time as tag
-            metric_data['execution_time'] = version_data['execution_time']
-            
-            json_body = [
-                {
-                    "measurement": "qualitymetrics",
-                    "tags": {
-                        'mode': 'cgp',
-                        "hash": version_data['revision'],
-                        'softmasking': version_data['softmasking'],
-                        'testcase': os.path.basename(metric_file).replace('.json', '')
-                    },
-                    "time": version_data['commit_date'],
-                    "fields": metric_data
-                }
-            ]
-
-            client.write_points(json_body)
-            # print(json_body)
 
 
 def expand_dir(path):
@@ -138,11 +110,8 @@ if __name__ == '__main__':
         print('Given data folder ' +  datadir + ' not found!')
         sys.exit()
 
-    if args.mode == 'single':
-        write_single(datadir)
-    elif args.mode == 'cgp':
-        write_cgp(datadir)
-    else:
+    if not (args.mode == 'single' or args.mode == 'cgp'):
         print('The mode must be \'cgp\' or \'single\'.')
         sys.exit()
 
+    write(datadir, str(args.mode))
