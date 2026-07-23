a75d87145754c3ca726e8a7e907e95f8552874a0
lanceman999 <loconn13@jh.edu>
2026-07-23 14:53:20 -0400

    Removed code chunk that adds '_odb10' to the end of any specified BUSCO lineage

--- scripts/compleasm_to_hints.py
+++ scripts/compleasm_to_hints.py
@@ -123,10 +123,6 @@ def main():
         if not os.access(args.compleasm, os.X_OK):
             raise FileNotFoundError("compleasm is not executable")
         
-    # check whether the database has the ending odb_10, if not, add it
-    if not args.database.endswith("_odb10"):
-        args.database = args.database + "_odb10"
-
     # apply compleasm to genome file with run_subprocess and the database
     if args.scratch_dir is None:
         args.scratch_dir = "compleasm_genome_out"
